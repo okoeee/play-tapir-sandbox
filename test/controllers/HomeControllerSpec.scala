66279325b529c3155000430c355dcd623115ea73
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2024-08-19 23:00:49 +0900

    fmt

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,9 +1,10 @@
 package controllers
 
-import org.scalatestplus.play._
-import org.scalatestplus.play.guice._
-import play.api.test._
-import play.api.test.Helpers._
+import adapter.controllers.HomeController
+import org.scalatestplus.play.*
+import org.scalatestplus.play.guice.*
+import play.api.test.*
+import play.api.test.Helpers.*
 
 /**
  * Add your spec here.
