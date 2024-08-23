eccd3b767903b3aeb97f45a41dc6f360815bc60c
Kazuma Yokoe <kazuma.yokoe@nextbeat.net>
2024-08-23 17:55:58 +0900

    remove unused method

--- app/adapter/controllers/TodoController.scala
+++ app/adapter/controllers/TodoController.scala
@@ -5,7 +5,6 @@ import domain.service.TodoQueryService
 
 import javax.inject.*
 import play.api.*
-import play.api.libs.json.Json
 import play.api.mvc.*
 
 import scala.concurrent.Future
@@ -15,14 +14,6 @@ class TodoController @Inject() (
     val controllerComponents: ControllerComponents,
     todoQueryService:         TodoQueryService
 ) extends BaseController:
-
-  def findByIdOld(id: Long): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
-    todoQueryService
-      .findById(id)
-      .map { todo =>
-        Ok(Json.toJson(JsValueTodo(todo)))
-      } getOrElse Ok(Json.toJson(JsValueError.notFound("Todo")))
-  }
 
   def findById(id: Long): Future[Either[JsValueError, JsValueTodo]] = {
     Future.successful {
